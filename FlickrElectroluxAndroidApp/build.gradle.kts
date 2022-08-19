46b25b4dbb511a6de41dfde5299547a389590c20
AjinkyaJD <Ajinkya@popmeals.com>
2022-08-19 20:27:02 +0800

    feature-render-photos-for-user : Added Jetpack compose required libraries
    -- Updated Kotlin version to 1.7.0 to make it compatible with required libraries

--- FlickrElectroluxAndroidApp/build.gradle.kts
+++ FlickrElectroluxAndroidApp/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 32
     defaultConfig {
         applicationId = "com.ajinkyad.flickrelectrolux.android"
         minSdk = 21
@@ -17,6 +17,22 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    // Set both the Java and Kotlin compilers to target Java 8.
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
@@ -24,4 +40,19 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.5.1")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:1.2.0")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
+    implementation("androidx.compose.runtime:runtime-livedata:1.2.0")
+
+    implementation("io.coil-kt:coil-compose:2.2.0")
+
+    // Compatible with Compose Material, includes MdcTheme
+    implementation("com.google.android.material:compose-theme-adapter:1.1.16")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.0")
     }
 }
-
 allprojects {
     repositories {
         google()
