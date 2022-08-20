4503b5e3314ffe943b4c0b13c2ad02e1f5384344
AjinkyaJD <Ajinkya@popmeals.com>
2022-08-20 10:22:32 +0800

    feature-add-search-field : Added custom Tag to be search in Flickr API.

--- shared/src/commonMain/kotlin/com/ajinkyad/flickrelectrolux/data/remote/services/FlickrImageService.kt
+++ shared/src/commonMain/kotlin/com/ajinkyad/flickrelectrolux/data/remote/services/FlickrImageService.kt
@@ -2,14 +2,10 @@ package com.ajinkyad.flickrelectrolux.data.remote.services
 
 import com.ajinkyad.flickrelectrolux.data.remote.base.CustomException
 import com.ajinkyad.flickrelectrolux.data.remote.base.Either
-import com.ajinkyad.flickrelectrolux.data.remote.dto.PhotoResponse
-import com.ajinkyad.flickrelectrolux.data.remote.dto.PhotosResponse
-import com.ajinkyad.flickrelectrolux.data.remote.dto.PhotosResponseWrapper
 import com.ajinkyad.flickrelectrolux.domain.entity.Photo
-import io.ktor.client.statement.*
 
 interface FlickrImageService {
 
     @Throws(Exception::class)
-    suspend fun fetchPhotos(): Either<CustomException, List<Photo>?>?
+    suspend fun fetchPhotos(query: String?): Either<CustomException, List<Photo>?>?
 }
